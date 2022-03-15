a97cb7a9304b3fa626878db001096bfdb6f31162
Hanan-cloud <79918951+Hanan-cloud@users.noreply.github.com>
2022-03-15 11:07:45 +0300

    moving wave

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,40 +1,64 @@
   import Phaser from "phaser";
   export default class Game extends Phaser.Scene{
 
-
-    preload(){
-
-    
+   
+    init(){
+      this.number=1;
+      this.timer;
     }
 
     create() {
-      this.graphics = this.add.graphics();
-    
-      
-      this.plotSine();
+      this.graph = this.add.graphics();
+      this.graph.lineStyle(2,0x00ffff);
+
+      //this.startD();
+     // this.time.delayedCall(10000,this.clearDraw() ,null,this);
+    }
+    update(){
+
+      this.plotSine(this.number);
+      //this.graph.clear();
+      this.number=this.number+0.1;
+      this.timer = this.time.delayedCall(300, this.startD, [], this);
+     
+     
     }
     
-    plotSine() {
-      this.graphics.lineStyle(2,0x00ffff);
-      
-      
-      
-    
-     
+     plotSine() {
       var x=0;
       var y=0;
-      this.graphics.moveTo(x,y);
-      var amplitude = 40;
+      this.graph.moveTo(x,y);
+      var amplitude = 30;
       var frequency = 20;
-      //ctx.moveTo(x, y);
+     //console.log("num="+this.number);
       while (x < window.innerWidth) {
-          y = window.innerHeight/2 + amplitude * Math.sin(x/frequency);
-            this.graphics.lineTo(x,y);
-         
+        
+          y = window.innerHeight/2 + amplitude * Math.sin(x/frequency-this.number);
+            this.graph.lineTo(x,y);
           x = x + 1;
-      }
-      this.graphics.strokePath();
-  }
+        
+      
+        }
+      
+      this.graph.strokePath();
+     
+      
+    }
+
+    clearDraw(){
+
+      console.log("i am in ");
+      this.graph.clear();
+    }
+    startD(){
+      this.timer = this.time.addEvent({
+        delay: 1000,                // ms
+        callback: this.clearDraw(),
+       
+        loop: true
+    });
+    
+    }
 
 
   }
